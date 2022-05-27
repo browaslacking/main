3de68042f1871a65922b938988ef38deb950c515
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-26 22:05:23 -0400

    Update load.lua

--- load.lua
+++ load.lua
@@ -2,26 +2,32 @@ game.StarterGui:SetCore(
     "SendNotification",
     {
 	Title = "Neverlocking.wtf",
-	Text = "Legit menu loading",
+	Text = "Thanks for using Never lockin",
 }
 )
-loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/better/main/notis/a.lua"))()
-local main1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/better/main/gui/guiC.lua"))()
-local main2 = main1.CreateLib("Neverlocking.wtf", "Midnight")
+local main1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/gui/main/aa.lua"))()
+local main2 = main1.CreateLib("Neverlocking.wtf Legit", "Midnight")
 local main3 = main2:NewTab("Aimbot")
-local main4 = main3:NewSection("Aimlock")
-main4:NewButton("Tenaki assist", "Tenakis aim assist ( V )", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/aimbot/main/sas.lua"))()
-end)
-main4:NewButton("Silent Aim", " 50% hit chance", function()
+local main4 = main3:NewSection("Streamproof")
+main4:NewButton("stream proof silent", "50% hit chance", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/aimbot/main/streamproof1.lua"))()
+end)
+main4:NewButton("Aim Assist", "Key is V", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/aimbot/main/sas.lua"))()
 end)
 local main28 = main2:NewTab("Misc")
 local main29 = main28:NewSection("Menu key")
 main29:NewKeybind(
     "Keybind Gui Toggle",
-    "LeftCtrl",
+    "h",
     Enum.KeyCode.RightControl,
     function()
 	main1:ToggleUI()
+end)
+local main29 = main28:NewSection("credits")
+main29:NewButton("Flash, tenaki, thusky", "a", function()
+				
+end)
+main29:NewButton("cynical, asher, miami", "a", function()
+				
 end)
