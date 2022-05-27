24716fc73cb0c0d986aca7982d78873d8b60dcd6
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-26 22:00:24 -0400

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,3 +1,10 @@
+game.StarterGui:SetCore(
+    "SendNotification",
+    {
+	Title = "Neverlocking.wtf",
+	Text = "Legit menu loading",
+}
+)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/better/main/notis/a.lua"))()
 local main1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/better/main/gui/guiC.lua"))()
 local main2 = main1.CreateLib("Neverlocking.wtf", "Midnight")
